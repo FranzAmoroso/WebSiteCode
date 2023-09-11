90a43620ed16202912176890c19c0c3b6326f896
FranzAmoroso <140825233+FranzAmoroso@users.noreply.github.com>
2023-09-12 00:56:22 +0200

    ADD NEW PAG WHIT ARGOMENTATION

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
-let simbolo 
-console.log(simbolo);
+let minimo = 1;
+let massimo = 10;
+let numeroCasuale =
+  Math.floor(Math.random() * (massimo - minimo + 1)) + minimo;
+console.log("Il numero casuale è: " + numeroCasuale);
